4566ae54fcb1f95c8d0dbdae60341e1c663b6f18
saif <saif@10.126.47.130>
2016-10-26 11:30:00 +0200

    Der er kommet connection fra MES TIL ERP Og ændring i TestgreenHouse, hvor der vil blive forbundet med klient

--- src/GreenhouseAPI/TestGreenhouse.java
+++ src/GreenhouseAPI/TestGreenhouse.java
@@ -5,15 +5,21 @@
  */
 package GreenhouseAPI;
 
+import MES.RMI_Config;
 import PLCCommunication.*;
+import com.sun.org.apache.xalan.internal.utils.FeatureManager;
 import java.io.IOException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.BitSet;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * API tester
@@ -21,35 +27,67 @@ import javafx.stage.Stage;
  * @author sps Testing
  */
 public class TestGreenhouse {
+    PLCConnection con = new UDPConnection(5000, "192.168.0.10");
+    private IGreenhouse greenhouse;
+    IGreenhouse api = new Greenhouse(con);
+    
+    private TestGreenhouse() throws RemoteException{
 
-    public static void main(String[] args) throws RemoteException {
+		String host = JOptionPane.showInputDialog("Server name?" , "localhost");
+		Registry registry;
 
+		try {
+			registry = LocateRegistry.getRegistry(host, RMI_Config.REGISTRY_PORT);
+			greenhouse = (IGreenhouse)registry.lookup(RMI_Config.OBJECT_NAME);
+		} catch(RemoteException | NotBoundException e) {
 
+			throw new Error("Error" + e);
+		}
+
+	}
+    
+    private void red() throws RemoteException {
+        api.SetRedLight(10);
+        
+    
+}
+    
+    
+ public static void main(String[] args) throws RemoteException {
+
+     TestGreenhouse t = new TestGreenhouse();
+     t.red();
 
 //        PLCConnection con = new UDPConnection(1025, "localhost"); 
-        PLCConnection con = new UDPConnection(5000, "192.168.0.10");
+        
 //        PLCConnection con = new SerialConnection("COM4");
 //        SerialConnection.getPortList("COM1");
 
-        IGreenhouse api = new Greenhouse(con);
-        int i = 0;
-        while (i != 200) {
-            api.SetRedLight(i);
-            i++;
-            System.out.println("hell");
-            //this
-        }
-
-        api.SetBlueLight(100);
-        //api.SetTemperature(273 + 25);
-        api.SetFanSpeed(1);
-        //double outdoorTemperature; 
-        //while (true)
-        //   outdoorTemperature = api.ReadTemp2();
-
-//        System.exit(3);
+        
+//        int i = 0;
+//        while (i != 200) {
+//            api.SetRedLight(i);
+//            i++;
+//            System.out.println("hell");
+//            //this
+//        }
+//
+//        api.SetBlueLight(100);
+//        //api.SetTemperature(273 + 25);
+//        api.SetFanSpeed(1);
+//        //double outdoorTemperature; 
+//        //while (true)
+//        //   outdoorTemperature = api.ReadTemp2();
+//
+////        System.exit(3);
     }
+            
+    }
+        
+    
+
+   
     
 
 
-}
+
--- src/MES/ERP_Connect.java
+++ src/MES/ERP_Connect.java
@@ -4,82 +4,74 @@
  * and open the template in the editor.
  */
 package MES;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+ 
+import java.sql.*;
+import java.util.logging.Logger;
+ 
 /**
  *
  * @author Morten
  */
 public class ERP_Connect {
-
+ 
     private Connection con = null;
-
+ 
     public static void main(String[] args) {
 //        ERP_Connect obj = new ERP_Connect();
 //        obj.run(args);
         ERP_Connect obj2 = new ERP_Connect();
-        obj2.getConnection();
-
+        Connection conn = obj2.getConnection();
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute("SELECT ITEMID, NAME, QTYCALC, SCHEDSTART, SCHEDEND FROM PRODTABLE WHERE DATAAREAID = 't074'");
+            ResultSet rs = stmt.getResultSet();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+ 
+        } catch (SQLException ex) {
+            //Logger.getLogger(ERP_Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
+ 
     public Connection getConnection() {
-
+ 
         try {
             // The atributes of the server
-            //  jdbc:sqlserver:[localhost[\instanceName][:portNumber]][;property=value[;property=value]]; 
-            String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; // NetDirect JDBC driver 
+            //  jdbc:sqlserver:[localhost[\instanceName][:portNumber]][;property=value[;property=value]];
+            String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; // NetDirect JDBC driver
             String serverName = "10.137.0.21";
             String portNumber = "1433";
             String mydatabase = serverName + ":" + portNumber + ";databaseName=Dynamics09";
-            String url = "jdbc:sqlserver://" + mydatabase + ";;user=AXReader;password=AXReader"; // a JDBC url 
+            String url = "jdbc:sqlserver://" + mydatabase + ";;user=AXReader;password=AXReader"; // a JDBC url
             System.out.println("Connecting to database... " + "(" + url + ")");
-
+           
             try {
-// Load the JDBC driver 
+// Load the JDBC driver
                 Class.forName(driverName);
             } catch (ClassNotFoundException e) {
                 System.out.println(e);
             }
-
-// Create a connection to the database 
+ 
+// Create a connection to the database
             DriverManager.setLoginTimeout(1);
             con = DriverManager.getConnection(url);
-
+ 
         } catch (SQLException e) {
-
+ 
             System.err.println("No connection to database... Using hardcoded values instead!");
             System.out.println(e);
-// Could not find the database driver 
+// Could not find the database driver
         }
-
-        getInfo(con);
+       
         return con;
-
-    }
-
-    public static void getInfo(Connection con) {
-        try {
-            PreparedStatement st = con.prepareStatement("SELECT * FROM INVENTTESTITEMQUALITYGROUP ");
-
-            ResultSet result = st.executeQuery();
-
-            while (result.next()) {
-
-                System.out.print(" Name: " + result.getString(""));
-//                System.out.println(" Amount: " + result.getString("current_stock"));
-
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-
-        }
-        System.out.println("");
-
+ 
     }
 }
